2f0be3f5e698906fdd4cd1c00b0d6282209f7a2c
Krzysztof <krysbaran@gmail.com>
2018-11-12 22:47:57 +0100

    Add tests and fix buggy code for gHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,22 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    GHappy gHappy = new GHappy();
+
+    @ParameterizedTest
+    @CsvSource({"xxggxx", "bdsagg", "ggbdsa", "ggggggg"})
+    public void testTrueCases(String testString) {
+        Assertions.assertTrue(gHappy.gHappy(testString));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"xxgxx", "xxggyygxx", "gbdsa", "bdsag"})
+    public void testFalseCases(String testString) {
+        Assertions.assertFalse(gHappy.gHappy(testString));
+    }
+
 }
